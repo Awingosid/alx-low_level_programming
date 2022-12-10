f9c4ea390402d4becde3b1393c00354d1fbadd0d
Godfred Awingosida Adoari <awingosid@gmail.com>
2022-12-10 05:56:02 -0800

    io

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and 0\n", n, b);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, b);
 	}
 
 	return (0);
