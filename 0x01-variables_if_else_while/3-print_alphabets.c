452bca83bd396199da30ffe0423d9d98d5a41663
Godfred Awingosida Adoari <awingosid@gmail.com>
2022-12-10 06:32:15 -0800

    i

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for (ch = 'A'; ch <= 'z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 
 	putchar('\n');
