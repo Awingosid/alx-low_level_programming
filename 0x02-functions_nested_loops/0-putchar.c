d9fbf772ab79ffcf047a5bccf7bac69df728071e
Godfred Awingosida Adoari <awingosid@gmail.com>
2022-12-14 07:27:21 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - prints _putchar and a new line
+ *Return: always 0
+ */
+int main(void)
+{
+  _putchar('_');
+  _putchar('p');
+  _Putchar('u');
+  _Putchar('t');
+  _Putchar('c');
+  _Putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+
+  return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+projects for 0x02. C Functions, nested loops
