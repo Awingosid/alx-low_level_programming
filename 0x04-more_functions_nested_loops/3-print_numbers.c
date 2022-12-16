3a336cf4af7f3811b28a350abcc51ee3168c9b1f
Godfred Awingosida Adoari <awingosid@gmail.com>
2022-12-16 02:48:18 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	int c;
+	char c;
 
-	for (c = 48; c <= 58; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-	putchar(c);
+		_putchar(c);
 	}
-	putchar(10);
+	_putchar('\n');
 }
